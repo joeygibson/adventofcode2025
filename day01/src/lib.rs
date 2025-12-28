6f5fd190ac5c46f9384a69118073795af38911b1
Joey Gibson <joey@joeygibson.com>
2025-12-28 16:01:21 -0500

    day 1, part 2 - Rust

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -48,6 +48,45 @@ fn part1(lines: Vec<String>) -> u64 {
     res.1 as u64
 }
 
+fn part2(lines: Vec<String>) -> u64 {
+    let instructions = parse_lines(lines);
+
+    let res = instructions
+        .iter()
+        .fold((50i64, 0i64), |acc, (dir, clicks)| {
+            let (current, mut zeros) = acc;
+
+            let round_trips = (clicks / 100) as i64;
+            zeros = zeros + round_trips;
+
+            let mut remaining_clicks = (clicks % 100) as i64;
+
+            if *dir == 'L' {
+                remaining_clicks = -remaining_clicks;
+            }
+
+            let mut tmp = current + remaining_clicks;
+
+            if tmp < 0 {
+                tmp = tmp + 100;
+                if current != 0 {
+                    zeros = zeros + 1;
+                }
+            } else if tmp > 99 {
+                tmp = tmp - 100;
+                if current != 0 {
+                    zeros = zeros + 1;
+                }
+            } else if tmp == 0 {
+                zeros = zeros + 1;
+            }
+
+            (tmp, zeros)
+        });
+
+    res.1 as u64
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -65,5 +104,19 @@ mod tests {
         let lines = load_data("input1.txt");
         let result = part1(lines);
         assert_eq!(1031, result);
+    }
+
+    #[test]
+    fn test_part_2_with_sample_data() {
+        let lines = load_data("input0.txt");
+        let result = part2(lines);
+        assert_eq!(6, result);
+    }
+
+    #[test]
+    fn test_part_2_with_real_data() {
+        let lines = load_data("input1.txt");
+        let result = part2(lines);
+        assert_eq!(5831, result);
     }
 }
